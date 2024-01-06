26e803da7a2f637813d4c52b23e8e788d5013d0c
2ndZorua <485892@student.fontys.nl>
2024-01-06 17:19:07 +0100

    Initial setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang=ts>
+    import "../app.css";
+</script>
+
+<svelte:head>
+    <title>Svelte-Carousel</title>
+</svelte:head>
+
+<slot></slot>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	mode: 'jit',
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+    screens: {
+			xs: { max: '576px' },
+			sm: { max: '768px' },
+			md: { max: '992px' },
+			lg: { max: '1200px' },
+			xl: { max: '1440px' },
+		}
+  },
+  plugins: [],
+}
