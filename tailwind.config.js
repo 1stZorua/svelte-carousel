51f073ceecaa6ea9491d004b3a6919ee594f3330
2ndZorua <485892@student.fontys.nl>
2024-01-11 20:03:16 +0100

    Commented screen variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
-    screens: {
-			xs: { max: '576px' },
-			sm: { max: '768px' },
-			md: { max: '992px' },
-			lg: { max: '1200px' },
-			xl: { max: '1440px' },
-		}
+    // screens: {
+	// 		xs: { max: '576px' },
+	// 		sm: { max: '768px' },
+	// 		md: { max: '992px' },
+	// 		lg: { max: '1200px' },
+	// 		xl: { max: '1440px' },
+	// 	}
   },
   plugins: [],
 }
