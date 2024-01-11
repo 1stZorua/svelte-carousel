8b41427358a24a73d79f0b4c0dff8bf67fd7d35e
2ndZorua <485892@student.fontys.nl>
2024-01-11 20:12:08 +0100

    Updated index page temporarily

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,48 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang=ts>
+    import { Carousel } from "$lib/index";
+
+    let test: number = 0;
+    let x: number = 0;
+</script>
+
+<Carousel.Root
+    class="gap-20"
+    itemOptions={{
+        inView: 2,
+        toScroll: 3
+    }}
+    bind:activeSlide={test}
+>
+    <div class="absolute -top-10 text-3xl">{test}</div>
+    {#each Array(6) as _, i}
+        <Carousel.Item 
+            item={{ 
+                type: "image", 
+                src: `https://source.unsplash.com/random/${i}`, 
+                alt: "Houndour"
+            }}
+            index={i}
+        ></Carousel.Item>
+    {/each}
+</Carousel.Root>
+
+<!-- <Carousel.Root
+    itemOptions={{
+        inView: 2,
+        toScroll: 3
+    }}
+    loop
+    bind:activeSlide={x}
+>
+    <div class="absolute -top-10 text-3xl">{x}</div>
+    {#each Array(6) as _, i}
+        <Carousel.Item 
+            item={{ 
+                type: "image", 
+                src: `https://source.unsplash.com/random/${i}`, 
+                alt: "Houndour"
+            }}
+            index={i}
+        ></Carousel.Item>
+    {/each}
+</Carousel.Root> -->
